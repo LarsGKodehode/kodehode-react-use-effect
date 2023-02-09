11e52c4a1d0c18e150405c6823331f12bd402617
LarsG <lars.ga.solheim@gmail.com>
2023-02-09 11:15:09 +0100

    comment cleanup

--- src/components/CleanupEffect/CleanupEffect.jsx
+++ src/components/CleanupEffect/CleanupEffect.jsx
@@ -3,9 +3,6 @@ import { useEffect, useState } from "react"
 function CleanupEffect() {
   const [count, setCount] = useState(0)
 
-  /**
-   * 
-   */
   function incrementCount() {
     setCount(
       (oldCount) => oldCount + 1
@@ -14,14 +11,16 @@ function CleanupEffect() {
 
   useEffect(
     () => {
-      // Tells the window object that we want some code (callback)
-      // to run at certain intervals
+      // Tells the window object that we want some code (the callback)
+      // to run at certain intervals, this is outside of React
       const intervalID = setInterval(
         () => incrementCount(),
         1000
       )
 
-      // Cleanup function to our interval
+      // setInterval will keep running, even if this component is destroyed
+      // So we nead some way to cleanup after our self
+      // This is what the returned callback is used for
       return () => {
         clearInterval(intervalID)
       }
--- src/components/FetchComponent/FetchComponent.jsx
+++ src/components/FetchComponent/FetchComponent.jsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react"
 
 const API_URL = "https://pokeapi.co/api/v2/pokemon"
 
+// To keep things inside the component nice and simple
+// we have extracted the logic for doing the fetch out here
 async function fetchData(callback) {
   const response = await fetch(API_URL)
   const parsedData = await response.json()
@@ -17,6 +19,8 @@ function FetchComponent() {
   useEffect(
     // A callback for the logic, if you need to do cleanup return another callback
     () => {fetchData(setData)},
+    // We only want to fetch data once
+    // so we are giving it an empty dependencies array
     []
   )
 
